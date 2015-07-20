443aa9e79c3b2703cfc1786ab05f618cdc4be7ad
Cristina Rodriguez Fernandez <CrisRodFe@macbook-pro-de-cristina.local>
2015-07-20 21:04:23 +0200

    Initial Commit

---
+++ AnimalTrivial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnimalTrivial
+//
+//  Created by Cristina Rodriguez Fernandez on 20/7/15.
+//  Copyright © 2015 Cristina Rodriguez Fernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
